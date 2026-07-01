8a76b531680d31455dba1c3256d8c1684c0100d3
Rougui SY <rougui080601@gmail.com>
2026-07-01 00:20:35 +0000

    Enregitrer categorie sans produits

--- tableaux.php
+++ tableaux.php
@@ -20,6 +20,51 @@
     foreach ($categories as  $categorie) {
         if (count($categorie["produits"])==0) {
             echo"nom :".$categorie["nom"]."\n";
-            echo" code :".$categorie["code"];
+            echo" code :".$categorie["code"]."\n";
         }
     }
+
+    // question3
+    do {
+        $codeValid = true;
+        $code = readline("Donnez un code :");
+        if ($code === "") {
+            echo"le code est obligatoir \n";
+            $codeValid = false;
+        }else {
+            for ($index=0; $index <count($categories) ; $index++) { 
+                if ($categories[$index]["code"]===$code) {
+                    echo"le code existe deja \n";
+                    $codeValid = false;
+                    break;
+                }
+            }
+        }
+    } while (!$codeValid);
+
+    do {
+
+        $nomValid = true;
+        $nom = readline("Donnez un nom :");
+        if ($nom === "") {
+            echo"le nom est obligatoir \n";
+            $nomValid = false;
+        }else {
+            for ($index=0; $index <count($categories) ; $index++) { 
+                if ($categories[$index]["nom"]===$nom) {
+                    echo"le nom existe deja \n";
+                    $nomValid = false;
+                    break;
+                }
+
+            }
+        }
+    } while (!$nomValid);
+
+    $categorie = [
+                "nom"=>$nom,
+                "code"=>$code,
+                "produits"=>[]
+    ];
+
+    $categories[]=$categorie;
